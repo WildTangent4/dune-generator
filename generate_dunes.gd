aae462c252269b86ac7a2aadbc1ab13eb0ff758e
WildTangent4 <wildtangent4@gmail.com>
2024-09-01 19:19:53 +0100

    updated function to support any size grid NOTE: still requires rotating the generation system to be flat and not vertical

--- generate_dunes.gd
+++ generate_dunes.gd
@@ -14,10 +14,11 @@ func _process(delta: float) -> void:
 	
 func init_mesh():
 	var vertices = PackedVector3Array()
-	create_base_mesh(2,2,vertices,0)
+	create_base_mesh(3,1,vertices,0)
 
 #add x * z points to a packed vector 3 array to make a flat plane
 func create_base_mesh(x_max,z_max,array,y_coordinate=0):
+	x_max = x_max+1 # need to create one more lines than the requested number of squares
 	var st = SurfaceTool.new()
 
 	st.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
@@ -28,18 +29,17 @@ func create_base_mesh(x_max,z_max,array,y_coordinate=0):
 
 	#The triangle strip primitive connects the next point to the previous two vertecies to form a triangle
 	#therefore to create one line of squares you need to make a series of vertical lines, these will be connected by the diagnoal lines |\|\|\|\|\|
-
-	for i in 10:
+	var y_max = z_max
+	for y in y_max:
+		for x in x_max:
 		#create straight edge
-		st.set_normal(Vector3(0, 0, 1))
-		st.set_uv(Vector2(0, 0))
-		st.add_vertex(Vector3(0, 0, i))
+			st.set_normal(Vector3(0, 0, 1))
+			st.set_uv(Vector2(0, 0))
+			st.add_vertex(Vector3(0, y, x))
 
-		st.set_normal(Vector3(0, 0, 1))
-		st.set_uv(Vector2(0, 1))
-		st.add_vertex(Vector3(0, 1, i))
-
-		
+			st.set_normal(Vector3(0, 0, 1))
+			st.set_uv(Vector2(0, 1))
+			st.add_vertex(Vector3(0, y+1, x))
 
 	
 	# Commit to a mesh.
