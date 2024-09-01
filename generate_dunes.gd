5bc5aec94cf99695587a56dfb1f03645761c9f07
WildTangent4 <wildtangent4@gmail.com>
2024-09-01 01:04:40 +0100

    added placeholder mesh for testing

--- generate_dunes.gd
+++ generate_dunes.gd
@@ -1,11 +1,26 @@
 extends MeshInstance3D
 
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	init_mesh()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func init_mesh():
+	var vertices = PackedVector3Array()
+	vertices.push_back(Vector3(0, 1, 0))
+	vertices.push_back(Vector3(1, 0, 0))
+	vertices.push_back(Vector3(0, 0, 1))
+
+	# Initialize the ArrayMesh.
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+
+	# Create the Mesh.
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
