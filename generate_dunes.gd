07cfa4403f7884a65b1faa869d2c6a274a90c493
WildTangent4 <wildtangent4@gmail.com>
2024-09-02 01:14:46 +0100

    added stub functions for applying sin and perlin noise to the grid, also improves formatting and removes unneeded comment

--- generate_dunes.gd
+++ generate_dunes.gd
@@ -14,24 +14,21 @@ func _process(delta: float) -> void:
 	
 func init_mesh():
 	var vertices = PackedVector3Array()
-	create_base_mesh(3,1,vertices,0)
+	create_base_mesh(100,100,vertices,0)
 
 #add x * z points to a packed vector 3 array to make a flat plane
 func create_base_mesh(x_max,z_max,array,y_coordinate=0):
-	x_max = x_max+1 # need to create one more lines than the requested number of squares
-	var st = SurfaceTool.new()
-
-	st.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
-
-	#create a quad from two tris
-
-	#create top right triangle
-
+	x_max = x_max+1 # need to create one more edge than the requested number of squares to ensure the final sqaure is complete
+	
+	var pipeline = []
+	
 	#The triangle strip primitive connects the next point to the previous two vertecies to form a triangle
-	#therefore to create one line of squares you need to make a series of vertical lines, these will be connected by the diagnoal lines |\|\|\|\|\|
+	#therefore to create one line of squares you need to make a series of vertical lines, these will be connected by the diagonal lines |\|\|\|\|\|
+	
+	var st = SurfaceTool.new()
+	st.begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
 	for z in z_max:
 		for x in x_max:
-		#create straight edge
 			st.set_normal(Vector3(0, 0, 1))
 			st.set_uv(Vector2(0, 0))
 			st.add_vertex(Vector3(x, 0, z+1))
@@ -39,10 +36,18 @@ func create_base_mesh(x_max,z_max,array,y_coordinate=0):
 			st.set_normal(Vector3(0, 0, 1))
 			st.set_uv(Vector2(0, 1))
 			st.add_vertex(Vector3(x, 0, z))
-
-	
-	# Commit to a mesh.
-	#st.generate_normals()
+	# Commit changes to a mesh.
 	st.generate_tangents()
 	mesh = st.commit()
-	
+
+#add the amplitude of a sin wave to the y height of the current coorindate, multiplied by amplitude, accounting for the angle of the wave
+func apply_sin(grid_pos, wave_amplitude = 0 , wave_frequency = 1, wave_angle = 0) -> Vector3:
+	return Vector3.INF
+
+#add the intensity of perlin noise at coordinate x,z to the y height of the given grid pos
+func apply_perlin_noise(grid_pos,seed) -> Vector3:
+	return Vector3.INF
+
+#run all of the functions in the pipleine on the specified coordinate and return the coordinate with the updated y value
+func run_pipeline(coordinate,pipeline = []) -> Vector3:
+	return Vector3.INF
